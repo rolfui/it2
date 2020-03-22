a693b148a4f61937f4b8e6f4ffb0ef7be8ccd173
Rolf Ulrich Isachsen <rolf@isachsen.org>
2020-03-22 14:34:33 +0100

    oppdatering

---
+++ kap8/personer.js
@@ -0,0 +1,13 @@
+var personer=
+`[
+    {"firstName":"Elvis","lastName":"Presley"},
+    {"firstName":"Roy","lastName":"Orbison"},
+    {"firstName":"Albert","lastName":"Einstein"},
+    {"firstName":"2","lastName":"Pac"},
+    {"firstName":"2","lastName":"Pac"},
+    {"firstName":"2","lastName":"Pac"},
+    {"firstName":"2","lastName":"Pac"},
+    {"firstName":"2","lastName":"Pac"},
+    {"firstName":"2","lastName":"Pac"}, 
+    {"firstName":"2","lastName":"Pac"}
+]` 
